2a4b2ff4e55eaf75028c9882ca2ddfcad77c6b37
NicoJMichel <nicolas.michel@enersis.ch>
2021-10-12 17:01:38 +0200

    feat(JS): API call mit effekt auf Button

--- api-call/api-call.js
+++ api-call/api-call.js
@@ -2,13 +2,24 @@
 
 let getBackground = function () {
   let getBodyElement = document.querySelector("body");
+  let getBodyHeight = document.body.clientHeight;
+  let getBodyWidth = document.body.clientWidth;
   var request = new XMLHttpRequest();
 
-  request.open("GET", "https://source.unsplash.com/random/");
+  request.open(
+    "GET",
+    "https://source.unsplash.com/random/" + getBodyWidth + "x" + getBodyHeight
+  );
   request.addEventListener("load", function (event) {
     if (request.status >= 200 && request.status < 300) {
       getBodyElement.style.backgroundImage = "url(" + request.responseURL + ")";
       console.log(request.responseURL);
+      console.log(
+        "https://source.unsplash.com/random/" +
+          getBodyWidth +
+          "x" +
+          getBodyHeight
+      );
     } else {
       console.warn(request.statusText, request.responseText);
     }
