c4fcc4d5762a2d6adb5ee8e41db2ad9c5f4ad29a
NicoJMichel <nicolas.michel@enersis.ch>
2021-10-13 11:15:19 +0200

    feat(JS): jest test

--- vanillajs/vanilla-dropdown.js
+++ vanillajs/vanilla-dropdown.js
@@ -4,84 +4,83 @@
 
 let navStatus = false;
 
-let toggleStatus = function() {
-    let getSidebar = document.querySelector(".sidebar");
-    let getSidebarUl = document.querySelector(".sidebar ul");
-    let getSidebarLinks = document.querySelectorAll(".sidebar a");
+let toggleStatus = function () {
+  let getSidebar = document.querySelector(".sidebar");
+  let getSidebarUl = document.querySelector(".sidebar ul");
+  let getSidebarLinks = document.querySelectorAll(".sidebar a");
 
-    if (navStatus === false) { // if Sidebar is closed 
-        closeMenu();
-        getSidebarUl.style.visibility = "visible";
-        getSidebar.style.width = "272px"; // change width of sidebar so the content can fit 
+  if (navStatus === false) {
+    // if Sidebar is closed
+    closeMenu();
+    getSidebarUl.style.visibility = "visible";
+    getSidebar.style.width = "272px"; // change width of sidebar so the content can fit
 
-        let arrayLength = getSidebarLinks.length;
-        for (let i = 0; i < arrayLength; i++) { // Smake every List item Visible
-            getSidebarLinks[i].style.opacity = "1";            
-        }
-
-        navStatus = true;
-        ;
+    let arrayLength = getSidebarLinks.length;
+    for (let i = 0; i < arrayLength; i++) {
+      // Smake every List item Visible
+      getSidebarLinks[i].style.opacity = "1";
     }
-    else { // if Sidebar is open 
-        getSidebarUl.style.visibility = "hidden";
-        getSidebar.style.width = "50px"; // change width of sidebar to the base Design 
 
-        let arrayLength = getSidebarLinks.length;
-        for (let i = 0; i < arrayLength; i++) { // make every List item invisible 
-            getSidebarLinks[i].style.opacity = "0";            
-        }
+    navStatus = true;
+  } else {
+    // if Sidebar is open
+    getSidebarUl.style.visibility = "hidden";
+    getSidebar.style.width = "50px"; // change width of sidebar to the base Design
 
-        navStatus = false;
+    let arrayLength = getSidebarLinks.length;
+    for (let i = 0; i < arrayLength; i++) {
+      // make every List item invisible
+      getSidebarLinks[i].style.opacity = "0";
     }
-}
+
+    navStatus = false;
+  }
+};
 
 /** ------------------------------------------------------------------------------------------------------------------ */
-
 
 /** Dropdown Menu in Top-Navigation, made by myself */
 
 let dropdownStatus = false;
 
-let toggleMenu = function() {
-    let getDropdownContainer = document.querySelector(".dropdown");
-    let getDropdownLinks = document.querySelectorAll(".listItem");
+let toggleMenu = function () {
+  let getDropdownContainer = document.querySelector(".dropdown");
+  let getDropdownLinks = document.querySelectorAll(".listItem");
 
-    if (dropdownStatus === false) { // if dropdown is closed 
-        closeMenu();
+  if (dropdownStatus === false) {
+    // if dropdown is closed
+    closeMenu();
 
-        let arrayLength = getDropdownLinks.length;
-        for (let i = 0; i < arrayLength; i++) {
-            getDropdownLinks[i].style.visibility = "visible"; // make items Visible
-        }
-
-        dropdownStatus = true;
-        
-        
-    } else { // if dropdown is open 
-        getDropdownContainer.style.background = "none";
-
-        let arrayLength = getDropdownLinks.length;
-        for (let i = 0; i < arrayLength; i++) {
-            getDropdownLinks[i].style.visibility = "hidden"; // make items invisible 
-        }
-
-        dropdownStatus = false;
+    let arrayLength = getDropdownLinks.length;
+    for (let i = 0; i < arrayLength; i++) {
+      getDropdownLinks[i].style.visibility = "visible"; // make items Visible
     }
-    
-}
+
+    dropdownStatus = true;
+  } else {
+    // if dropdown is open
+    getDropdownContainer.style.background = "none";
+
+    let arrayLength = getDropdownLinks.length;
+    for (let i = 0; i < arrayLength; i++) {
+      getDropdownLinks[i].style.visibility = "hidden"; // make items invisible
+    }
+
+    dropdownStatus = false;
+  }
+};
 
 /** ------------------------------------------------------------------------------------------------------------------ */
 
 /** Close any other Menu which is open at the time of opening a new one  */
 
-let closeMenu = function() {
-    if ( dropdownStatus === true) {
-        toggleMenu(); // close Dropdown if sidebar gets opened
-    }else if (navStatus === true) {
-        toggleStatus(); // close Sidebar if Dropdown gets opened
-    }
-
-}
+let closeMenu = function () {
+  if (dropdownStatus === true) {
+    toggleMenu(); // close Dropdown if sidebar gets opened
+  } else if (navStatus === true) {
+    toggleStatus(); // close Sidebar if Dropdown gets opened
+  }
+};
 
 /** Close any Menu if user Clicks somewhere else 
 
@@ -103,3 +102,11 @@ let clickCheck = function() {
     }   
 */
 
+/** ------------------------------------------------------------------------------------------------------------------ */
+
+/** jest test function */
+
+function sum(a, b) {
+  return a + b;
+}
+module.exports = sum;
