48161b3d5d10140ea8355a871727924a0c8d181f
NicoJMichel <nicolas.michel@enersis.ch>
2021-10-18 19:39:12 +0200

    feat(JS): get nice info of the city

--- teleport-api/teleport.js
+++ teleport-api/teleport.js
@@ -3,8 +3,8 @@
 /** Get input from input field */
 
 let searchCity = function () {
-  let input = document.getElementById("city").value;
-  let getContainerElement = document.querySelector("response-container");
+  let input = document.getElementById("city").value; //get user input
+
   var request = new XMLHttpRequest();
   let cityResponse;
 
@@ -15,22 +15,40 @@ let searchCity = function () {
   request.addEventListener("load", function (event) {
     if (request.status >= 200 && request.status < 300) {
       cityResponse = request.responseText;
-      getGeoNameID();
+      getGeoNameID(cityResponse); //call next function with the response
     } else {
       console.warn(request.statusText, request.responseText);
     }
   });
   request.send();
+};
 
-  let getGeoNameID = function () {
-    let parsedCityResponse = JSON.parse(cityResponse);
-    let embeddedObject = parsedCityResponse._embedded;
-    let firstResult = embeddedObject["city:search-results"][0];
+/** get the ID of the best matching result city by using reading the handed over response */
+let getGeoNameID = function (cityResponse) {
+  let parsedCityResponse = JSON.parse(cityResponse); //parse the response
+  let embeddedObject = parsedCityResponse._embedded;
+  let firstResult = embeddedObject["city:search-results"][0]; //get the first entry of the response
 
-    console.log(firstResult);
+  let cityLink = firstResult["_links"]["city:item"]["href"]; //get the link to the city out of entry
 
-    let cityLink = firstResult["_links"]["city:item"]["href"];
+  let geoNameID = cityLink.substr(36, 18); //get the ID out of the link
 
-    console.log(cityLink);
-  };
+  console.log(geoNameID);
+
+  getCityInformation(geoNameID); //call next function with ID as parameter
+};
+
+/** get Information about City by ID */
+let getCityInformation = function (geoNameID) {
+  var request = new XMLHttpRequest();
+  /** request to get information of city by geonameid */
+  request.open("GET", "https://api.teleport.org/api/cities/" + geoNameID);
+  request.addEventListener("load", function (event) {
+    if (request.status >= 200 && request.status < 300) {
+      console.log(JSON.parse(request.responseText));
+    } else {
+      console.warn(request.statusText, request.responseText);
+    }
+  });
+  request.send();
 };
