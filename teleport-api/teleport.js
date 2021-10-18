6dc6b200611a9da0bf0467252169e0bd1224c440
NicoJMichel <nicolas.michel@enersis.ch>
2021-10-18 19:03:52 +0200

    feat(JS): get city link of user input city name

--- teleport-api/teleport.js
+++ teleport-api/teleport.js
@@ -1,28 +1,36 @@
-/** Get a background image from the unsplash API  */
+/** Get information from teleport api of a specific city  */
 
-let getBackground = function () {
-  let getBodyElement = document.querySelector("body");
-  let getBodyHeight = document.body.clientHeight;
-  let getBodyWidth = document.body.clientWidth;
+/** Get input from input field */
+
+let searchCity = function () {
+  let input = document.getElementById("city").value;
+  let getContainerElement = document.querySelector("response-container");
   var request = new XMLHttpRequest();
+  let cityResponse;
 
-  request.open(
-    "GET",
-    "https://source.unsplash.com/random/" + getBodyWidth + "x" + getBodyHeight
-  );
+  console.log(input);
+
+  /** request to get geonameid of input name */
+  request.open("GET", "https://api.teleport.org/api/cities/?search=" + input);
   request.addEventListener("load", function (event) {
     if (request.status >= 200 && request.status < 300) {
-      getBodyElement.style.backgroundImage = "url(" + request.responseURL + ")";
-      console.log(request.responseURL);
-      console.log(
-        "https://source.unsplash.com/random/" +
-          getBodyWidth +
-          "x" +
-          getBodyHeight
-      );
+      cityResponse = request.responseText;
+      getGeoNameID();
     } else {
       console.warn(request.statusText, request.responseText);
     }
   });
   request.send();
+
+  let getGeoNameID = function () {
+    let parsedCityResponse = JSON.parse(cityResponse);
+    let embeddedObject = parsedCityResponse._embedded;
+    let firstResult = embeddedObject["city:search-results"][0];
+
+    console.log(firstResult);
+
+    let cityLink = firstResult["_links"]["city:item"]["href"];
+
+    console.log(cityLink);
+  };
 };
